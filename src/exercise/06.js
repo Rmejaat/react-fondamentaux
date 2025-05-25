505f3ebe815f8f99b2c401a3a53f290609321746
Rayan Mejaat <142797698+HandiReaper@users.noreply.github.com>
2025-05-25 18:47:43 +0200

    Exercice 6 Bonus 1

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -6,28 +6,46 @@
 // Si isAdmin est false alors on affichera que le bouton 'Lire'
 // 🤖 ButtonActions({isAdmin})
 
-const buttonRead = <button>Lire</button>
-const buttonCreate = <button>Créer</button>
-const buttonUpdate = <button>Modifier</button>
-const buttonDelete = <button>Supprimer</button>
+// const buttonRead = <button>Lire</button>
+// const buttonCreate = <button>Créer</button>
+// const buttonUpdate = <button>Modifier</button>
+// const buttonDelete = <button>Supprimer</button>
+
+const AdminActions = () => {
+  return (
+    <>
+      <button>Lire</button>
+      <button>Créer</button>
+      <button>Modifier</button>
+      <button>Supprimer</button>
+    </>
+  )
+}
+
+const GuestActions = () => {
+  return (
+    <>
+      <button>Lire</button>
+    </>
+  )
+}
 
 const ButtonActions = ({ isAdmin }) => {
   return isAdmin ? (
     <>
-      {buttonRead}
-      {buttonCreate}
-      {buttonUpdate}
-      {buttonDelete}
+      <AdminActions />
     </>
   ) : (
-    <> {buttonRead}</>
+    <>
+      <GuestActions />
+    </>
   )
 }
 
 function App() {
   return (
     <div>
-      <ButtonActions isAdmin={true} />
+      <ButtonActions isAdmin={false} />
     </div>
   )
 }
