4a4a9dd7b46c40c1a475bed746545dcf4ad80352
Rayan Mejaat <142797698+HandiReaper@users.noreply.github.com>
2025-05-25 18:56:53 +0200

    Exercice 7

--- src/exercise/07.js
+++ src/exercise/07.js
@@ -1,20 +1,18 @@
 // Rendu de tableaux
 // http://localhost:3000/alone/exercise/07.js
 
-import * as React from 'react'
-
 // 🐶 Le code suivant génère un warning :  Warning: Each child in a list should have a unique "key" prop
 
 const skills = []
 // 🐶 Corrige ce code avec des keys uniques sur les elements <li>
-skills.push(<li>HTML</li>)
-skills.push(<li>CSS</li>)
-skills.push(<li>JS</li>)
-skills.push(<li>REACT</li>)
-skills.push(<li>ANGULAR</li>)
+skills.push(<li key="HTML">HTML</li>)
+skills.push(<li key="CSS">CSS</li>)
+skills.push(<li key="JS">JS</li>)
+skills.push(<li key="REACT">REACT</li>)
+skills.push(<li key="ANGULAR">ANGULAR</li>)
 
 function MesSkills() {
-  return <ul>{skills.map(skill => skill)}</ul>
+  return <ul>{skills.map((skill) => skill)}</ul>
 }
 
 function App() {
