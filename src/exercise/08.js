393ed6cbf2891b5fe79bf3bf4221c86c880789c1
Rayan Mejaat <142797698+HandiReaper@users.noreply.github.com>
2025-05-25 19:45:49 +0200

    Exercice 8

--- src/exercise/08.js
+++ src/exercise/08.js
@@ -2,32 +2,37 @@
 // Les States
 // http://localhost:3000/alone/exercise/08.js
 
-import * as React from 'react'
-
 // 🐶 Créé un fonction 'addSkillPython' qui ajoute le skill Python
 // 🤖 skills.push({id:'xxxx', value: 'Python'})
 // 🤖 Utilise {id:`${Math.random()}` pour générer un id aléatoire
 // 🐶 dans la fonction 'addSkillPython' logue le tableau skills
 // 🤖 console.log('skills',skills)
 
+const addSkillPython = () => {
+  const id = Math.floor(Math.random() * 1000)
+  skills.push({ id, value: "Python" })
+  console.log("skills", skills)
+}
+
 const skills = [
-  {id: 'e313', value: 'HTML'},
-  {id: 'f980', value: 'CSS'},
-  {id: '11eb', value: 'JS'},
-  {id: '9a03', value: 'REACT'},
-  {id: '65d4', value: 'ANGULAR'},
+  { id: "e313", value: "HTML" },
+  { id: "f980", value: "CSS" },
+  { id: "11eb", value: "JS" },
+  { id: "9a03", value: "REACT" },
+  { id: "65d4", value: "ANGULAR" },
 ]
 
 // 🐶 Créé un button dans le render et sur l'evenement onclick passer la fonction 'addSkillPython'
 // 🤖 <button onClick={addSkillPython}
-function MesSkills() {
+const MesSkills = () => {
   return (
     <>
       <ul>
-        {skills.map(skill => (
+        {skills.map((skill) => (
           <li key={skill.id}>{skill.value}</li>
         ))}
       </ul>
+      <button onClick={addSkillPython}>Add Skill</button>
     </>
   )
 }
